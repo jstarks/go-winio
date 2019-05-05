60cde84ba101ced3194dbbfcf7c6669d1939ba5a
John Starks <jostarks@microsoft.com>
2019-05-05 00:59:35 +0000

    pkg/etwlogrus: Allow event customization
    This change introduces several ways to customize the event that is
    written by the logrus ETW hook. The user can:
    
    * Set the default event name for events.
    * Request that the entry's message field be used as the event name.
    * Request that the event name come from a specific field from the entry.
    * Set the field name for the entry's message.

--- pkg/etwlogrus/hook.go
+++ pkg/etwlogrus/hook.go
@@ -9,6 +9,22 @@ import (
 
 // Hook is a Logrus hook which logs received events to ETW.
 type Hook struct {
+	// If EventFromMessage is true, then the entry's message is
+	// used as the event name.
+	EventFromMessage bool
+
+	// EventKey specifies the entry field used to determine the
+	// ETW event name. Defaults to "etw.event".
+	EventKey string
+
+	// DefaultEvent sets the event name for entries without EventKey.
+	// Defaults to "LogrusEntry".
+	DefaultEvent string
+
+	// MessageField sets the ETW field name for the entry's message.
+	// Only used if EventFromMessage is false. Defaults to "Message".
+	MessageField string
+
 	provider      *etw.Provider
 	closeProvider bool
 }
@@ -20,14 +36,20 @@ func NewHook(providerName string) (*Hook, error) {
 	if err != nil {
 		return nil, err
 	}
-
-	return &Hook{provider, true}, nil
+	h, _ := NewHookFromProvider(provider)
+	h.closeProvider = true
+	return h, nil
 }
 
 // NewHookFromProvider creates a new hook based on an existing ETW provider. The
 // provider will not be closed when the hook is closed.
 func NewHookFromProvider(provider *etw.Provider) (*Hook, error) {
-	return &Hook{provider, false}, nil
+	return &Hook{
+		provider:     provider,
+		EventKey:     "etw.event",
+		DefaultEvent: "LogrusEntry",
+		MessageField: "Message",
+	}, nil
 }
 
 // Levels returns the set of levels that this hook wants to receive log entries
@@ -64,24 +86,41 @@ func (h *Hook) Fire(e *logrus.Entry) error {
 		return nil
 	}
 
+	nfields := 0
+	eventName := h.DefaultEvent
+	if h.EventFromMessage {
+		eventName = e.Message
+	} else {
+		// Reserve extra space for the message field.
+		nfields++
+	}
 	// Sort the fields by name so they are consistent in each instance
 	// of an event. Otherwise, the fields don't line up in WPA.
 	names := make([]string, 0, len(e.Data))
 	var errv interface{}
 	for k := range e.Data {
-		names := make([]string, 0, len(e.Data))
-		if k == logrus.ErrorKey {
-			// Always put the error last because it is optional in some events.
+		switch k {
+		case h.EventKey:
+			if s, ok := e.Data[k].(string); ok {
+				eventName = s
+			}
+		case logrus.ErrorKey:
+			// Save the error in order to put it last because
+			// some events tend to have this field only sometimes,
+			// and it would otherwise mix up the order of fields.
 			errv = e.Data[k]
-		} else {
+			nfields++
+		default:
 			names = append(names, k)
+			nfields++
 		}
 	}
 	sort.Strings(names)
 
-	// Reserve extra space for the message field.
-	fields := make([]etw.FieldOpt, 0, len(e.Data)+1)
-	fields = append(fields, etw.StringField("Message", e.Message))
+	fields := make([]etw.FieldOpt, 0, nfields)
+	if !h.EventFromMessage {
+		fields = append(fields, etw.StringField(h.MessageField, e.Message))
+	}
 	for _, k := range names {
 		fields = append(fields, etw.SmartField(k, e.Data[k]))
 	}
@@ -90,7 +129,7 @@ func (h *Hook) Fire(e *logrus.Entry) error {
 	}
 
 	return h.provider.WriteEvent(
-		"LogrusEntry",
+		eventName,
 		etw.WithEventOpts(etw.WithLevel(level)),
 		fields)
 }
